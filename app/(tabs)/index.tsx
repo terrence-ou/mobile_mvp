e22265109d268f462ff2d1225947d2b2e0a367d2
terrence-ou <outerrencedl@gmail.com>
2024-12-24 18:17:16 -0500

    mvp updated

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,15 +1,20 @@
-import { Text, View, StyleSheet } from "react-native";
-import { Link } from "expo-router";
+import { View, StyleSheet } from "react-native";
+
+import Button from "@/components/Button";
+import ImageViewer from "@/components/ImageViewer";
+
+const placeholderImage = require("@/assets/images/background-image.png");
 
 export default function Index() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>
-        Edit app/index.tsx to edit this screen.
-      </Text>
-      <Link href="/about" style={styles.button}>
-        Go to About Screen
-      </Link>
+      <View style={styles.imageContainer}>
+        <ImageViewer imgSource={placeholderImage} />
+      </View>
+      <View style={styles.footerContainer}>
+        <Button label="Choose a phote" theme="primary" />
+        <Button label="Use this photo" />
+      </View>
     </View>
   );
 }
@@ -19,14 +24,13 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: "#25292e",
     alignItems: "center",
-    justifyContent: "center",
   },
-  text: {
-    color: "#fff",
+  imageContainer: {
+    flex: 1,
+    paddingTop: 28,
   },
-  button: {
-    fontSize: 20,
-    textDecorationLine: "underline",
-    color: "#fff",
+  footerContainer: {
+    flex: 1 / 3,
+    alignItems: "center",
   },
 });
