7fc0ea606954c0724cd42de8e871de439ebae77b
terrence-ou <outerrencedl@gmail.com>
2024-12-30 18:39:44 -0500

    sign in page updated

--- app/sign-in.tsx
+++ app/sign-in.tsx
@@ -8,26 +8,36 @@ import ViewWrapper from "@/components/ViewWrapper";
 
 export default function SignIn() {
   const { signIn } = useSession();
+
+  const handleAppleSignIn = async () => {
+    try {
+      const identityToken = await appleSignIn();
+      if (identityToken) {
+        signIn(identityToken);
+        router.replace("/dictionary");
+      } else {
+        throw new Error("Apple Sign-In failed - no identify token returned");
+      }
+    } catch (error) {
+      alert(`Apple Sign-In Error: ${error}`);
+    }
+  };
+
   return (
     <ViewWrapper>
       <View style={styles.container}>
+        <View style={styles.subContainer}>
+          <Text style={styles.title}>Jiten</Text>
+          <Text style={styles.subTitle}>
+            Jiten is a <Text>FREE</Text> multi-language-supported dictionary app. The
+            reason for the registration is to avoid misused by strange bots.
+          </Text>
+        </View>
         <AppleButton
           buttonStyle={AppleButton.Style.BLACK}
           buttonType={AppleButton.Type.CONTINUE}
-          style={{ width: 240, height: 40 }}
-          onPress={() =>
-            appleSignIn()
-              .then((sessionToken) => {
-                if (sessionToken) {
-                  signIn(sessionToken);
-                  router.replace("/dictionary");
-                }
-              })
-              .catch((error) => {
-                // You can also show an error message to the user here
-                alert(`Apple Sign-In Error: ${error}`);
-              })
-          }
+          style={{ width: 280, height: 45 }}
+          onPress={handleAppleSignIn}
         />
       </View>
     </ViewWrapper>
@@ -39,5 +49,20 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
+  },
+  subContainer: {
+    marginVertical: 50,
+    paddingHorizontal: 20,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  title: {
+    fontSize: 55,
+    fontWeight: "bold",
+    marginVertical: 20,
+  },
+  subTitle: {
+    fontSize: 16,
+    textAlign: "left",
   },
 });
