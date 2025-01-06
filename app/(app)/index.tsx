530c68b5d6065556ed37a8293ecaeda846b77bb3
terrence-ou <outerrencedl@gmail.com>
2025-01-06 17:50:08 -0500

    package issue fixed

--- app/(app)/index.tsx
+++ app/(app)/index.tsx
@@ -1,17 +1,21 @@
-import { StyleSheet } from "react-native";
+import { StyleSheet, View } from "react-native";
 import { Word } from "@/models/word";
 import { mockWordList, mockWord } from "@/constants/mockData";
 import ViewWrapper from "@/components/ViewWrapper";
 import DictionaryHeader from "@/components/DictionaryHeader";
 import WordSlider from "@/components/WordSlider";
+import SignOutButton from "@/components/SignOutButton";
 
 export default function App() {
   return (
     <ViewWrapper>
-      <DictionaryHeader />
-      <WordSlider
-        words={[...mockWordList.words, mockWord].map((word) => new Word(word))}
-      />
+      <View style={styles.container}>
+        <DictionaryHeader />
+        <WordSlider
+          words={[...mockWordList.words, mockWord].map((word) => new Word(word))}
+        />
+        <SignOutButton />
+      </View>
     </ViewWrapper>
   );
 }
