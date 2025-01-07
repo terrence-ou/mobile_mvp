c8ab112e7c0d061eced41ecba00208d0e9f32252
terrence-ou <outerrencedl@gmail.com>
2025-01-07 09:58:47 -0500

    mvp cleanup

--- atoms/storageAtom.ts
+++
@@ -1,8 +0,0 @@
-import { atomWithStorage, createJSONStorage } from "jotai/utils";
-import AsyncStorage from "@react-native-async-storage/async-storage";
-import { SESSION_KEY } from "@/constants/storageKeys";
-import { SessionStorage } from "@/models/storage";
-
-const storage = createJSONStorage<SessionStorage>(() => AsyncStorage);
-const content = { session_token: null };
-export const sessionAtom = atomWithStorage<SessionStorage>(SESSION_KEY, content, storage);
--- constants/storageKeys.ts
+++
@@ -1,2 +0,0 @@
-export const SESSION_KEY = "jiten_session";
-export const DATA_KEY = "jiten_saved_words";
